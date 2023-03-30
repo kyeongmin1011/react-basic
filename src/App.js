7815ef3e5857a885754392ba8a9bcfeb03a69482
kyeongmin1011 <kyeongmin@safety-as.com>
2023-03-30 14:39:23 +0900

    블로그 포스트 생성 폼 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import {useState} from "react";
 
 function App() {
+  const [inputText, setInputText] = useState({
+    title: '',
+    content: ''
+  })
+
+  const {title, content} = inputText
+
+  const handleInput = (e) => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const onSubmit = () => {
+    axios.post('http://localhost:3001/posts', {
+      title,
+      content
+    })
+    console.log(title, content)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <div className="mb-3">
+        <label className="form-label">Title</label>
+        <input type="text"
+               className="form-control"
+               value={title}
+               name="title"
+               onChange={handleInput}/>
+      </div>
+
+      <div className="mb-3">
+        <label className="form-label">Content</label>
+        <textarea className="form-control"
+                  rows="20"
+                  value={content}
+                  name="content"
+                  onChange={handleInput}/>
+      </div>
+
+      <button className="btn btn-primary" onClick={onSubmit}>Save</button>
     </div>
   );
 }
