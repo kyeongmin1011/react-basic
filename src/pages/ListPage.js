83a27400b321cfa8be67f5fc3107de50574ccb5a
kyeongmin1011 <kyeongmin@safety-as.com>
2023-04-03 13:26:26 +0900

    23. 블로그 리스트 데이터 받아오기 (useEffect)

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,5 +1,18 @@
-
+import axios from "axios";
+import {useEffect, useState} from "react";
 const ListPage = () => {
+  const [posts, setPosts] = useState([])
+
+  const getData = () => {
+    axios.get('http://localhost:3001/posts')
+      .then(res => setPosts(res.data))
+  }
+
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="ListPage">
       <h2>리스트</h2>
