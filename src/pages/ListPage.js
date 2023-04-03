9cd068b160f575e574182c36d0172e20fd217cab
kyeongmin1011 <kyeongmin@safety-as.com>
2023-04-03 13:34:20 +0900

    24. 받아온 데이터 화면에 출력하기

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
+
 const ListPage = () => {
   const [posts, setPosts] = useState([])
 
@@ -15,7 +16,15 @@ const ListPage = () => {
 
   return (
     <div className="ListPage">
-      <h2>리스트</h2>
+      <h1>Blogs</h1>
+      {posts.map(post => {
+        return (
+          <div key={post.id}>
+            <div>{post.title}</div>
+            <div>{post.content}</div>
+          </div>
+        )
+      })}
     </div>
   )
 }
