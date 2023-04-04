fedd5921f30e42a2fa5a1ac2530ba79f84c9d50f
kyeongmin1011 <kyeongmin@safety-as.com>
2023-04-04 13:17:58 +0900

    40. edit 페이지 route 추가

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -2,7 +2,7 @@ import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import LoadingSpinner from "../components/LoadingSpinner";
-
+import {Link} from "react-router-dom";
 
 const ShowPage = () => {
   const {id} = useParams()
@@ -30,7 +30,11 @@ const ShowPage = () => {
   }
   return (
     <div className="ShowPage container mt-3">
-      <h1>{post.title}</h1>
+      <div className="d-flex">
+        <h1 className="flex-grow-1">{post.title}</h1>
+        <div><Link className="btn btn-primary" to={`/blogs/${id}/edit`}>Edit</Link></div>
+      </div>
+
       <small className="text-muted">{printDate(post.createdAt)}</small>
       <hr/>
       <p>{post.content}</p>
